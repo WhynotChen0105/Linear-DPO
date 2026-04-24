df958e56e3607f34b95aa3ee9f47f5af56edeb7b
WhynotChen0105 <166620719+WhynotChen0105@users.noreply.github.com>
2026-04-24 14:11:28 +0800

    Update run_sd3_pickapic.sh

--- run_sd3_pickapic.sh
+++ run_sd3_pickapic.sh
@@ -1,14 +1,14 @@
 export HF_ENDPOINT=https://hf-mirror.com
-MODEL_NAME="/mnt/fuse/.cache/models--stabilityai--stable-diffusion-3-medium-diffusers/snapshots/ea42f8cef0f178587cf766dc8129abd379c90671"
-DATASET_NAME="/nas/zhiyi/data/HPDv3" # make sure "pickapic" in dataset_name 
+MODEL_NAME="stabilityai/stable-diffusion-3-medium-diffusers"
+DATASET_NAME="./data/HPDv3" # make sure "pickapic" in dataset_name 
 LR=5e-6
 WORLD_SIZE=8
 ACCUMULATION_STEPS=16
 BETA=1000
 BATCH_SIZE=$((WORLD_SIZE * ACCUMULATION_STEPS))
-CACHE_DIR="/nas/zhiyi/huggingface_cache/datasets"
+CACHE_DIR="./huggingface_cache/datasets"
 RUN_NAME="85k_SD3_HPDv3_lr${LR}_bs${BATCH_SIZE}_beta${BETA}"
-OUTPUT_DIR="/nas/zhiyi/output/Diffusers_sd3_dpo_new/${RUN_NAME}"
+OUTPUT_DIR="./outputs/Diffusers_sd3_dpo_new/${RUN_NAME}"
 
 accelerate launch train/train_sd3_dpo.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
