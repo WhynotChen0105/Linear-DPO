ad444dd9f11314ccb802f5936a63884041905780
WhynotChen0105 <166620719+WhynotChen0105@users.noreply.github.com>
2026-04-24 14:10:34 +0800

    Update model and dataset paths in run_sd1_5_pickapic.sh

--- run_sd1_5_pickapic.sh
+++ run_sd1_5_pickapic.sh
@@ -1,5 +1,5 @@
-export MODEL_NAME="/mnt/fuse/.cache/models--runwayml--stable-diffusion-v1-5/snapshots/1d0c4ebf6ff58a5caecab40fa1406526bca4b5b9"
-export DATASET_NAME="/nas/xinxuan/model/datasets--liuhuohuo2--pick-a-pic-v2/pickapic" # make sure "pickapic" in dataset_name 
+export MODEL_NAME="sd-legacy/stable-diffusion-v1-5"
+export DATASET_NAME="./datasets--liuhuohuo2--pick-a-pic-v2/pickapic" # make sure "pickapic" in dataset_name 
 export HF_ENDPOINT=https://hf-mirror.com
 
 LR=1e-8
@@ -7,9 +7,9 @@ WORLD_SIZE=8
 ACCUMULATION_STEPS=256
 BETA=2500
 BATCH_SIZE=$((WORLD_SIZE * ACCUMULATION_STEPS))
-CACHE_DIR="/nas/zhiyi/huggingface_cache/datasets"
+CACHE_DIR="./huggingface_cache/datasets"
 RUN_NAME="SD_PickaPic_lr${LR}_bs${BATCH_SIZE}_beta${BETA}"
-OUTPUT_DIR="/nas/zhiyi/output/Diffusers_sd_dpo/${RUN_NAME}"
+OUTPUT_DIR="./outputs/Diffusers_sd_dpo/${RUN_NAME}"
 
 accelerate launch --mixed_precision="fp16"  train/train_sd_dpo.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
@@ -29,4 +29,3 @@ accelerate launch --mixed_precision="fp16"  train/train_sd_dpo.py \
   --output_dir=$OUTPUT_DIR \
   --choice_model="pickscore" \
   --tracker_project_name=$RUN_NAME \
-  
